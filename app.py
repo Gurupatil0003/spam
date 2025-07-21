c0502bc97fba302472dfb5ab8cf70d61d3c77596
GURANNA GOUDA <110026505+Gurupatil0003@users.noreply.github.com>
2025-07-21 18:12:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
-from flask import Flask,render_template,request,jsonify
-import pandas as pd
-import numpy as np 
+from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
+# Load model
 model = joblib.load('spam_model.pkl')
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
-  if request.method == 'POST':
-    message = request.form.get('message')
-    output = model.predict([message])
-    if output == [0]:
-      result = "This Message is Not a SPAM Message."
-    else:
-      result = "This Message is a SPAM Message." 
-    return render_template('index.html', result=result,message=message)      
-
-  else:
-    return render_template('index.html')  
-
+    if request.method == 'POST':
+        message = request.form.get('message')
+        output = model.predict([message])
+        result = "This Message is a SPAM Message." if output == [1] else "This Message is Not a SPAM Message."
+        return render_template('index.html', result=result, message=message)
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
